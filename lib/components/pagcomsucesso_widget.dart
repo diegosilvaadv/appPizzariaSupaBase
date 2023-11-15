f7891640a59072e24c47ccab0d5b6e4bb80a2c5a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-15 12:26:52 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/components/pagcomsucesso_widget.dart
@@ -0,0 +1,130 @@
+import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'pagcomsucesso_model.dart';
+export 'pagcomsucesso_model.dart';
+
+class PagcomsucessoWidget extends StatefulWidget {
+  const PagcomsucessoWidget({Key? key}) : super(key: key);
+
+  @override
+  _PagcomsucessoWidgetState createState() => _PagcomsucessoWidgetState();
+}
+
+class _PagcomsucessoWidgetState extends State<PagcomsucessoWidget> {
+  late PagcomsucessoModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => PagcomsucessoModel());
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    context.watch<FFAppState>();
+
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: Text(
+                'Pagamento Realizado\n com Sucesso',
+                textAlign: TextAlign.center,
+                style: FlutterFlowTheme.of(context).titleMedium.override(
+                      fontFamily: 'Readex Pro',
+                      color: FlutterFlowTheme.of(context).primaryText,
+                      fontSize: 30.0,
+                    ),
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 0.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                splashColor: Colors.transparent,
+                focusColor: Colors.transparent,
+                hoverColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onTap: () async {
+                  setState(() {
+                    FFAppState().ProdutosDoCarrinho = [];
+                    FFAppState().totalprice = 0;
+                    FFAppState().numberCarrinho = 0;
+                  });
+
+                  context.goNamed('homepage');
+                },
+                child: Icon(
+                  Icons.home,
+                  color: FlutterFlowTheme.of(context).primaryText,
+                  size: 24.0,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(20.0, 0.0, 0.0, 0.0),
+                child: FFButtonWidget(
+                  onPressed: () async {
+                    setState(() {
+                      FFAppState().ProdutosDoCarrinho = [];
+                      FFAppState().totalprice = 0;
+                      FFAppState().numberCarrinho = 0;
+                    });
+
+                    context.goNamed('pedidos_cliente');
+                  },
+                  text: 'Acessar Meu Pedido',
+                  options: FFButtonOptions(
+                    height: 40.0,
+                    padding:
+                        EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
+                    iconPadding:
+                        EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                    color: FlutterFlowTheme.of(context).primary,
+                    textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                          fontFamily: 'Readex Pro',
+                          color: Colors.white,
+                        ),
+                    elevation: 3.0,
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
